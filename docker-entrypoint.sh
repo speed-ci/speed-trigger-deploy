a0c78302ada757e5665b41e3371f464b33f4a127
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-25 03:14:15 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -52,7 +52,8 @@ echo "PROJECT_NAME      : $PROJECT_NAME"
 echo "PROJECT_NAMESPACE : $PROJECT_NAMESPACE"
 
 PROJECT_DEPLOY_NAME="$PROJECT_NAMESPACE-deploy"
-PROJECT_DEPLOY_ID=`curl -s --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_DEPLOY_NAME"`
+PROJECT_DEPLOY_ID=`curl --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_DEPLOY_NAME"`
+echo "PROJECT_DEPLOY_ID : $PROJECT_DEPLOY_ID"
 
 if [[ -n $PROJECT_DEPLOY_ID ]];then
     printstep "Déclenchement du déploiement sur le projet $PROJECT_DEPLOY_NAME"
