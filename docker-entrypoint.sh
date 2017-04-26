305969773ff7b91dd5e1dac072ffcbe5bed79f6f
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-04-26 00:18:29 +0000

    fix(trigger): use single quote for jq query

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -62,7 +62,7 @@ PROJECT_DEPLOY_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_
 if [[ $PROJECT_DEPLOY_ID != "null" ]]; then
 
     printstep "Préparation du déclencheur trigger_deploy sur le projet $PROJECT_DEPLOY_NAME"
-    PIPELINE_TOKEN=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/triggers" | jq ".[] | select(.description == "trigger_deploy")" | jq .token`
+    PIPELINE_TOKEN=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/triggers" | jq '.[] | select(.description == "trigger_deploy")' | jq .token`
     if [[ -z $PIPELINE_TOKEN ]]; then
         printinfo "Création du déclencheur manquant trigger_deploy"
         PIPELINE_TOKEN=`curl --silent --noproxy '*' --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --form description="trigger_deploy" "$GITLAB_API_URL/projects/13/triggers" | jq .token`
