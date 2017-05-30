8cfc270a0bb9a532fee96db5bbc5958182882c5e
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-05-30 14:41:00 +1100

    fix(trigger): extract deploy project id from json result

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,7 +16,7 @@ echo "PROJECT_NAME        : $PROJECT_NAME"
 echo "PROJECT_NAMESPACE   : $PROJECT_NAMESPACE"
 echo "PROJECT_DEPLOY_NAME : $PROJECT_DEPLOY_NAME"
 
-PROJECT_DEPLOY_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_DEPLOY_NAME" | jq --arg project_namespace "$PROJECT_NAMESPACE" '.[] | select(.namespace.name == "\($project_namespace)")'`
+PROJECT_DEPLOY_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects?search=$PROJECT_DEPLOY_NAME" | jq --arg project_namespace "$PROJECT_NAMESPACE" '.[] | select(.namespace.name == "\($project_namespace)")' | jq .id`
 
 if [[ $PROJECT_DEPLOY_ID != "null" ]]; then
 
