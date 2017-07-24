35135d218eaec7aab1c4f8f216b2d5fa18288d3b
Mathieu Filotto <m.filotto@eramet-sln.nc>
2017-07-24 15:30:56 +1100

    refactor(curl): use myCurl to reveal hidden potential errors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,28 +24,28 @@ if [[ $PROJECT_DEPLOY_ID != "" ]]; then
     GITLAB_CI_USER_MEMBERSHIP=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/members?query=$GITLAB_CI_USER" | jq .[0]`
     if [[ $GITLAB_CI_USER_MEMBERSHIP == "null" ]]; then 
         printinfo "Ajout du user $GITLAB_CI_USER manquant au projet $PROJECT_NAMESPACE/$PROJECT_DEPLOY_NAME"
-        curl --silent --noproxy '*' --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/members" -d "user_id=$GITLAB_CI_USER_ID" -d "access_level=40"
+        myCurl --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/members" -d "user_id=$GITLAB_CI_USER_ID" -d "access_level=40"
     fi
     
     printstep "Préparation du déclencheur trigger_deploy sur le projet $PROJECT_NAMESPACE/$PROJECT_DEPLOY_NAME"
-    PIPELINE_TOKEN=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/triggers" | jq '.[] | select(.description == "trigger_deploy")' | jq -r .token`
+    PIPELINE_TOKEN=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/triggers" | jq '.[] | select(.description == "trigger_deploy")' | jq -r .token`
 
     if [[ -z $PIPELINE_TOKEN ]]; then
         printinfo "Création du déclencheur manquant trigger_deploy"
-        PIPELINE_TOKEN=`curl --silent --noproxy '*' --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --form description="trigger_deploy" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/triggers" | jq -r .token`
+        PIPELINE_TOKEN=`myCurl --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --form description="trigger_deploy" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/triggers" | jq -r .token`
     fi
 
     printstep "Déclenchement du déploiement sur le projet $PROJECT_NAMESPACE/$PROJECT_DEPLOY_NAME"
-    PIPELINE_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/trigger/pipeline" -d "token=$PIPELINE_TOKEN" -d "ref=$BRANCH_NAME" -d "variables[SERVICE_TO_UPDATE]=$PROJECT_NAME" -d "variables[TRIGGER_USER_ID]=$GITLAB_USER_ID" | jq .id `
+    PIPELINE_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/trigger/pipeline" -d "token=$PIPELINE_TOKEN" -d "ref=$BRANCH_NAME" -d "variables[SERVICE_TO_UPDATE]=$PROJECT_NAME" -d "variables[TRIGGER_USER_ID]=$GITLAB_USER_ID" | jq .id `
     sleep 5
     
-    DEPLOY_JOB_ID=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/pipelines/$PIPELINE_ID/jobs/" | jq '.[] | select(.name | startswith("deploy")) | .id'`
+    DEPLOY_JOB_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/pipelines/$PIPELINE_ID/jobs/" | jq '.[] | select(.name | startswith("deploy")) | .id'`
     
     if [[ $DEPLOY_JOB_ID != "" ]]; then
         
         while :
         do
-            JOB_STATUS=`curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs/$DEPLOY_JOB_ID" | jq -r .status`
+            JOB_STATUS=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs/$DEPLOY_JOB_ID" | jq -r .status`
             if [[ $JOB_STATUS != "pending" ]] && [[ $JOB_STATUS != "running" ]]; then break; fi
             sleep 5
         done
@@ -56,7 +56,7 @@ if [[ $PROJECT_DEPLOY_ID != "" ]]; then
         printinfo "Lien d'accès aux logs distants d'origine : $GITLAB_URL/$PROJECT_NAMESPACE/$PROJECT_DEPLOY_NAME/builds/$DEPLOY_JOB_ID"
         sleep 5
         echo ""
-        curl --silent --noproxy '*' --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs/$DEPLOY_JOB_ID/trace"
+        myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/jobs/$DEPLOY_JOB_ID/trace"
         if [[ $JOB_STATUS != "success" ]]; then exit 1; fi
 
     else
