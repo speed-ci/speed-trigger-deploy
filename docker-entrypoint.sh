00c654346e42c6d024a5c841629455e06f0fd5b4
Mathieu Filotto <m.filotto@eramet-sln.nc>
2018-07-03 14:33:27 +1100

    fix(speed-trigger-deploy): use DEPLOY_BRANCH_NAME var to trigger deploy projet

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -41,7 +41,7 @@ if [[ $PROJECT_DEPLOY_ID != "" ]]; then
     else
         DEPLOY_BRANCH_NAME=$BRANCH_NAME
     fi
-    PIPELINE_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/trigger/pipeline" -d "token=$PIPELINE_TOKEN" -d "ref=$BRANCH_NAME" -d "variables[SERVICE_TO_UPDATE]=$PROJECT_NAME" -d "variables[TRIGGER_USER_ID]=$GITLAB_USER_ID" | jq .id`
+    PIPELINE_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -XPOST "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/trigger/pipeline" -d "token=$PIPELINE_TOKEN" -d "ref=$DEPLOY_BRANCH_NAME" -d "variables[SERVICE_TO_UPDATE]=$PROJECT_NAME" -d "variables[TRIGGER_USER_ID]=$GITLAB_USER_ID" | jq .id`
     sleep 5
     
     DEPLOY_JOB_ID=`myCurl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_API_URL/projects/$PROJECT_DEPLOY_ID/pipelines/$PIPELINE_ID/jobs/" | jq '.[] | select(.name | startswith("deploy")) | .id'`
